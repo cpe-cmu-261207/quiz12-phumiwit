868e693404c6d7f413902c298068b5110052a639
phumiwit <68888298+phumiwit@users.noreply.github.com>
2021-01-12 13:58:04 +0700

    Add files via upload

---
+++ Quiz1 phumiwit 620612160/quiz12-phumiwit/app_template.js
@@ -0,0 +1,22 @@
+//your code here!
+    let num
+    let id  = document.getElementById('subtitle').value
+    let  inputnumber = document.getElementById('input-number').value
+    let button  = document.getElementById('button-compute').value
+
+    if( document.getElementById('input-number').checked)
+    {
+       let num = num+inputnumber;
+       console.log(num); 
+    }
+    let name;
+    let studentid;
+    let input;
+    let row = document.getElementById('input-row').value
+    let col = document.getElementById('input-col').value
+    let bottontable = document.getElementById('button-create-table').value
+  if(document.getElementById('button-create-table').checked) 
+   {
+       console.log('create by' + name , studentid)
+   }
+    
